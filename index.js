ea49adf8a183123b4e2de8b2d5f10f6d5df5bb19
juandejunin <juandejunin75@gmail.com>
2023-11-05 18:17:29 +0100

    feat: download-audio-webm

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const youtubeDl = require('youtube-dl-exec');
 const path = require('path');
-const fs = require('fs/promises'); // Usar fs.promises para trabajar con promesas.
+const fs = require('fs/promises');
 
 const url = 'https://www.youtube.com/watch?v=6cnFl9aHD5Y';
 const outputDir = './descargas/';
@@ -8,18 +8,17 @@ const outputDir = './descargas/';
 // Función para descargar el audio
 async function descargarAudio() {
   try {
-    // Verifica si el directorio de salida existe y créalo si es necesario.
     await fs.mkdir(outputDir, { recursive: true });
 
     const options = {
-      o: path.join(outputDir, '%(title)s.%(ext)s'), // Ruta de salida
-      format: 'bestaudio/best', // Descarga el mejor formato de audio disponible.
+      o: path.join(outputDir, '%(title)s.%(ext)s'),
+      format: 'bestaudio/best[ext=mp3]', // Descarga el mejor formato de audio en MP3.
     };
 
     const output = await youtubeDl(url, options);
-    console.log('Audio descargado:', output);
+    console.log('Audio MP3 descargado:', output);
   } catch (error) {
-    console.error('Error al descargar el audio:', error);
+    console.error('Error al descargar el audio MP3:', error);
   }
 }
 
