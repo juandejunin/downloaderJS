c7c55dc9f7041d2bc89c88ed11e3e0aa79b597df
juandejunin <juandejunin75@gmail.com>
2023-11-05 18:34:16 +0100

    feat: convertor mp3

--- index.js
+++ index.js
@@ -1,25 +1,80 @@
 const youtubeDl = require('youtube-dl-exec');
 const path = require('path');
 const fs = require('fs/promises');
+const axios = require('axios'); // Importa la biblioteca Axios.
+const ffmpeg = require('fluent-ffmpeg'); 
 
 const url = 'https://www.youtube.com/watch?v=6cnFl9aHD5Y';
 const outputDir = './descargas/';
 
-// Función para descargar el audio
-async function descargarAudio() {
+async function obtenerTituloLimpiado() {
   try {
-    await fs.mkdir(outputDir, { recursive: true });
-
-    const options = {
-      o: path.join(outputDir, '%(title)s.%(ext)s'),
-      format: 'bestaudio/best[ext=mp3]', // Descarga el mejor formato de audio en MP3.
-    };
-
-    const output = await youtubeDl(url, options);
-    console.log('Audio MP3 descargado:', output);
+    const response = await axios.get(url);
+    const titleMatch = response.data.match(/<title>([^<]*)<\/title>/);
+    if (titleMatch && titleMatch[1]) {
+      const rawTitle = titleMatch[1];
+      const cleanTitle = rawTitle.replace(/[^\w\s]/gi, '').replace(/\s+/g, '').trim().toLowerCase();
+      return cleanTitle;
+    } else {
+      throw new Error('No se pudo encontrar el título del video.');
+    }
   } catch (error) {
-    console.error('Error al descargar el audio MP3:', error);
+    console.error('Error al obtener el título del video:', error);
+    return null;
   }
 }
+
+// Función para obtener el título limpio de la canción desde la URL del video.
+// async function obtenerTituloLimpiado() {
+//   try {
+//     const response = await axios.get(url);
+//     const titleMatch = response.data.match(/<title>([^<]*)<\/title>/);
+//     if (titleMatch && titleMatch[1]) {
+//       const rawTitle = titleMatch[1];
+//       const cleanTitle = rawTitle.replace(/[^\w\s]/gi, '').replace(/\s+/g, ' ').trim().toLowerCase();
+//       console.log(cleanTitle)
+//       return cleanTitle;
+//     } else {
+//       throw new Error('No se pudo encontrar el título del video.');
+//     }
+//   } catch (error) {
+//     console.error('Error al obtener el título del video:', error);
+//     return null;
+//   }
+// }
+
+async function descargarAudio() {
+  const cleanTitle = await obtenerTituloLimpiado();
+
+  if (cleanTitle) {
+    try {
+      await fs.mkdir(outputDir, { recursive: true });
+
+      const webmOptions = {
+        o: path.join(outputDir, `${cleanTitle}.webm`),
+        format: 'bestaudio/best',
+      };
+
+      const webmOutput = await youtubeDl(url, webmOptions);
+      console.log('Audio webm descargado:', webmOutput);
+
+      const mp3Filename = path.join(outputDir, `${cleanTitle}.mp3`);
+
+      // Realiza la conversión a MP3 utilizando fluent-ffmpeg.
+      ffmpeg()
+        .input(path.join(outputDir, webmOutput[0]))
+        .audioCodec('libmp3lame')
+        .audioBitrate(320)
+        .outputOptions('-map_metadata 0')
+        .toFormat('mp3')
+        .on('end', () => console.log('Conversión a MP3 exitosa.'))
+        .on('error', (err) => console.error('Error en la conversión a MP3: ' + err))
+        .save(mp3Filename);
+    } catch (error) {
+      console.error('Error al descargar y convertir el audio a MP3:', error);
+    }
+  }
+}
+
 
 descargarAudio();
