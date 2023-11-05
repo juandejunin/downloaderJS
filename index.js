01c09e64b8bf7d06375f260c1ad07f50d2942417
juandejunin <juandejunin75@gmail.com>
2023-11-05 17:54:27 +0100

    feat: downlad mp4

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
 const youtubeDl = require('youtube-dl-exec');
-const { exec } = require('child_process');
 const path = require('path');
+const fs = require('fs/promises'); // Usar fs.promises para trabajar con promesas.
 
 const url = 'https://www.youtube.com/watch?v=6cnFl9aHD5Y';
-const outputDir = './descargas/'
-// Especifica que solo se debe descargar el audio en el mejor formato disponible.
-const options = {
-  o: `${outputDir}%(title)s.%(ext)s`, // Ruta de salida
-  format: 'bestaudio/best', // Descarga el mejor formato de audio disponible.
-};
+const outputDir = './descargas/';
 
-youtubeDl(url, options)
-  .then(output => {
+// Función para descargar el audio
+async function descargarAudio() {
+  try {
+    // Verifica si el directorio de salida existe y créalo si es necesario.
+    await fs.mkdir(outputDir, { recursive: true });
+
+    const options = {
+      o: path.join(outputDir, '%(title)s.%(ext)s'), // Ruta de salida
+      format: 'bestaudio/best', // Descarga el mejor formato de audio disponible.
+    };
+
+    const output = await youtubeDl(url, options);
     console.log('Audio descargado:', output);
-    
-
-    // Ruta del archivo de audio descargado y ruta de salida en formato MP3.
-    const audioFilePath = `${outputDir}titulo.${output.ext}`;
-    const mp3OutputPath = `${outputDir}titulo.mp3`;
-    // Comando de conversión a MP3 usando ffmpeg.
-    const convertCommand = `ffmpeg -i "${audioFilePath}" "${mp3OutputPath}"`;
-    exec(convertCommand, (error, stdout, stderr) => {
-        if (error) {
-          console.error('Error al convertir a MP3:', error);
-        } else {
-          console.log('Conversión a MP3 exitosa:', mp3OutputPath);
-        }
-      });
-  })
-  .catch(error => {
+  } catch (error) {
     console.error('Error al descargar el audio:', error);
-  });
+  }
+}
+
+descargarAudio();
