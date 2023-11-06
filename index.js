798fcaf8d8073a2cc0e422c43067a21fb05fd28c
juandejunin <juandejunin75@gmail.com>
2023-11-06 05:23:06 +0100

    feat:  successful mp3 conversion

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const youtubeDl = require('youtube-dl-exec');
 const path = require('path');
 const fs = require('fs/promises');
 const axios = require('axios'); // Importa la biblioteca Axios.
-const ffmpeg = require('fluent-ffmpeg'); 
+const ffmpeg = require('fluent-ffmpeg');
 
 const url = 'https://www.youtube.com/watch?v=6cnFl9aHD5Y';
 const outputDir = './descargas/';
@@ -24,27 +24,10 @@ async function obtenerTituloLimpiado() {
   }
 }
 
-// Función para obtener el título limpio de la canción desde la URL del video.
-// async function obtenerTituloLimpiado() {
-//   try {
-//     const response = await axios.get(url);
-//     const titleMatch = response.data.match(/<title>([^<]*)<\/title>/);
-//     if (titleMatch && titleMatch[1]) {
-//       const rawTitle = titleMatch[1];
-//       const cleanTitle = rawTitle.replace(/[^\w\s]/gi, '').replace(/\s+/g, ' ').trim().toLowerCase();
-//       console.log(cleanTitle)
-//       return cleanTitle;
-//     } else {
-//       throw new Error('No se pudo encontrar el título del video.');
-//     }
-//   } catch (error) {
-//     console.error('Error al obtener el título del video:', error);
-//     return null;
-//   }
-// }
 
 async function descargarAudio() {
   const cleanTitle = await obtenerTituloLimpiado();
+  console.log('titulo procesado: ' + cleanTitle)
 
   if (cleanTitle) {
     try {
@@ -53,16 +36,19 @@ async function descargarAudio() {
       const webmOptions = {
         o: path.join(outputDir, `${cleanTitle}.webm`),
         format: 'bestaudio/best',
+
       };
 
       const webmOutput = await youtubeDl(url, webmOptions);
+
       console.log('Audio webm descargado:', webmOutput);
 
       const mp3Filename = path.join(outputDir, `${cleanTitle}.mp3`);
 
+      console.log(mp3Filename)
       // Realiza la conversión a MP3 utilizando fluent-ffmpeg.
       ffmpeg()
-        .input(path.join(outputDir, webmOutput[0]))
+        .input(path.join(outputDir, `${cleanTitle}.webm`))
         .audioCodec('libmp3lame')
         .audioBitrate(320)
         .outputOptions('-map_metadata 0')
